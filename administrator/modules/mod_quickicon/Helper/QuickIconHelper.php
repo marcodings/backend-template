b13ebd463503f89500bde2c116a18f9a45d3e13c
chmst <dev@chmst.de>
2019-05-03 14:35:04 +0200

    Module quickicons with splitted icons - WIP

--- administrator/modules/mod_quickicon/Helper/QuickIconHelper.php
+++ administrator/modules/mod_quickicon/Helper/QuickIconHelper.php
@@ -68,152 +68,15 @@ abstract class QuickIconHelper
 
 		if (!isset(self::$buttons[$key]))
 		{
-			$context = $params->get('context', 'mod_quickicon');
-
-			self::$buttons[$key] = [];
-
-			if ($context === 'mod_quickicon')
-			{
-				// Load mod_quickicon language file in case this method is called before rendering the module
-				$application->getLanguage()->load('mod_quickicon');
-				
-				if ($params->get('show_global', '1'))
-				{				
-					self::$buttons[$key][] = [
-						'link'   => Route::_('index.php?option=com_config'),
-						'image'  => 'fa fa-cog',
-						'text'   => Text::_('MOD_QUICKICON_GLOBAL_CONFIGURATION'),
-						'access' => array('core.manage', 'com_config', 'core.admin', 'com_config'),
-						'group'  => 'MOD_QUICKICON_CONFIGURATION',
-					];
-				}
-
-				if ($params->get('show_users', '1'))
-				{
-					$amount = self::countUsers();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_users'),
-						'linkadd'   => Route::_('index.php?option=com_users&task=user.add'),
-						'addwhat' => Text::plural('MOD_QUICKICON_USER_MANAGER', 1),
-						'name'   => Text::plural('MOD_QUICKICON_USER_MANAGER', $amount),
-						'access' => array('core.manage', 'com_users', 'core.create', 'com_users'),
-						'group'  => 'MOD_QUICKICON_USERS',
-					];
-				}
-
-				if ($params->get('show_menuItems', '1'))
-				{
-					$amount = self::countMenuItems();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_menus'),
-						'linkadd'   => Route::_('index.php?option=com_menus&task=item.add'),
-						'addwhat' => Text::plural('MOD_QUICKICON_MENUITEMS_MANAGER', 1),
-						'name'   => Text::plural('MOD_QUICKICON_MENUITEMS_MANAGER', $amount),
-						'access' => array('core.manage', 'com_menus', 'core.create', 'com_menus'),
-						'group'  => 'MOD_QUICKICON_STRUCTURE',
-					];
-				}
-
-				if ($params->get('show_articles', '1'))
-				{
-					$amount = self::countArticles();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_content'),
-						'linkadd'   => Route::_('index.php?option=com_content&task=article.add'),
-						'addwhat' => Text::plural('MOD_QUICKICON_ARTICLE_MANAGER', 1),
-						'name'   => Text::plural('MOD_QUICKICON_ARTICLE_MANAGER', $amount),
-						'access' => array('core.manage', 'com_content', 'core.create', 'com_content'),
-						'group'  => 'MOD_QUICKICON_CONTENT',
-					];
-				}
-		
-				if ($params->get('show_categories', '1'))
-				{
-					$amount = self::countArticleCategories();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_categories'),
-						'addwhat' => Text::plural('MOD_QUICKICON_CATEGORY_MANAGER', 1),
-						'linkadd'   => Route::_('index.php?option=com_categories&task=category.add'),
-						'name'   => Text::plural('MOD_QUICKICON_CATEGORY_MANAGER', $amount),
-						'access' => array('core.manage', 'com_categories', 'core.create', 'com_categories'),
-						'group'  => 'MOD_QUICKICON_CONTENT',
-					];
-				}
-
-				if ($params->get('show_media', '1'))
-				{
-					self::$buttons[$key][] = [
-						'image'  => 'fa fa-image',
-						'link'   => Route::_('index.php?option=com_media'),
-						'text'   => Text::_('MOD_QUICKICON_MEDIA_MANAGER'),
-						'access' => array('core.manage', 'com_media'),
-						'group'  => 'MOD_QUICKICON_CONTENT',
-					];
-				}
-
-				if ($params->get('show_modules', '1'))
-				{
-					$amount = self::countModules();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_modules'),
-						'text'   => Text::plural('MOD_QUICKICON_MODULE_MANAGER', $amount),
-						'access' => array('core.manage', 'com_modules'),
-						'group'  => 'MOD_QUICKICON_CONTENT'
-					];
-				}
-
-				if ($params->get('show_plugins', '1'))
-				{
-					$amount = self::countPlugins();
-					
-					self::$buttons[$key][] = [
-						'amount' => $amount,
-						'link'   => Route::_('index.php?option=com_plugins'),
-						'text'   => Text::plural('MOD_QUICKICON_PLUGIN_MANAGER', $amount),
-						'access' => array('core.manage', 'com_plugins'),
-						'group'  => 'MOD_QUICKICON_MAINTENANCE'
-					];
-				}
-
-				if ($params->get('show_checkin', '1'))
-				{
-					self::$buttons[$key][] = [
-						'amount' => self::countCheckin(),
-						'link'   => Route::_('index.php?option=com_checkin'),
-						'text'   => Text::_('MOD_QUICKICON_CHECKINS'),
-						'access' => array('core.admin', 'com_checkin'),
-						'group'  => 'MOD_QUICKICON_CONTENT'
-					];
-				}
-
-				if ($params->get('show_templates', '1'))
-				{
-					self::$buttons[$key][] = [
-						'amount' => self::countTemplates(),
-						'link'   => Route::_('index.php?option=com_templates&client_id=0'),
-						'text'   => Text::_('MOD_QUICKICON_TEMPLATES'),
-						'access' => array('core.admin', 'com_templates'),
-						'group'  => 'MOD_QUICKICON_TEMPLATES'
-					];
-				}
-			}
+			// Load mod_quickicon language file in case this method is called before rendering the module
+			$application->getLanguage()->load('mod_quickicon');
 
 			// Include buttons defined by published quickicon plugins
 			PluginHelper::importPlugin('quickicon');
 
 			$arrays = (array) $application->triggerEvent(
 				'onGetIcons',
-				new QuickIconsEvent('onGetIcons', ['context' => $context])
+				new QuickIconsEvent('onGetIcons', [$params->get('show_extensionupdate', '1')])
 			);
 
 			foreach ($arrays as $response)
@@ -221,14 +84,11 @@ abstract class QuickIconHelper
 				foreach ($response as $icon)
 				{
 					$default = array(
-						'amount' => null,
 						'link'   => null,
-						'addwhat'   => null,
-						'linkadd'   => null,
-						'name' => null,
 						'image'  => null,
 						'text'   => null,
 						'access' => true,
+						'class' => true,
 						'group'  => 'MOD_QUICKICON_EXTENSIONS',
 					);
 					$icon = array_merge($default, $icon);
@@ -239,10 +99,209 @@ abstract class QuickIconHelper
 					}
 				}
 			}
-		}
 
-		return self::$buttons[$key];
+			if ($params->get('show_checkin', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-unlock',
+					'amount' => self::countCheckin(),
+//'ajaxurl' => 'index.php?option=com_checkin&amp;task=getMenuBadgeData&amp;format=json',
+					'link'   => Route::_('index.php?option=com_checkin'),
+					'text'   => Text::_('MOD_QUICKICON_CHECKIN'),
+					'access' => array('core.admin', 'com_checkin'),
+					'link2'   => Route::_('index.php?option=com_checkin'),
+					'image2'  => 'fa fa-cog',
+					'text2'   => Text::_('MOD_QUICKICON_CHECKIN_ALL'),
+					'access2' => array('core.admin', 'com_checkin')
+				];
+			}
+			
+			if ($params->get('show_cache', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-cloud',
+					'link'   => Route::_('index.php?option=com_cache'),
+					'text'   => Text::_('MOD_QUICKICON_CACHE'),
+					'access' => array('core.admin', 'com_cache'),
+					'class' => 'cache',
+					'link2'   => Route::_('index.php?option=com_ache&task=cache.empty'),
+					'image2'  => 'fa fa-remove',
+					'text2'   => Text::_('MOD_QUICKICON_CACHE_CLEAR'),
+					'access2' => array('core.admin', 'com_cache')
+				];
+			}
+				
+			if ($params->get('show_global', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-cog',
+					'link'   => Route::_('index.php?option=com_config'),
+					'text'   => Text::_('MOD_QUICKICON_GLOBAL'),
+					'access' => array('core.admin', 'com_config'),
+					'class' => 'configuration-color',
+				];
+			}
+				
+			if ($params->get('show_extensions', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-puzzle-piece',
+					'amount' => self::countExtensions(),
+//'ajaxurl' => 'index.php?option=com_checkin&amp;task=getMenuBadgeData&amp;format=json',
+					'link'   => Route::_('index.php?option=com_installer&view=manage'),
+					'text'   => Text::_('MOD_QUICKICON_EXTENSIONS'),
+					'access' => array('core.admin', 'com_installer'),
+					'link2'   => Route::_('index.php?option=com_installer&view=install'),
+					'image2'  => 'fa fa-cog',
+					'text2'   => Text::_('MOD_QUICKICON_EXTENSIONS_INSTALL'),
+					'access2' => array('core.admin', 'com_checkin')
+				];
+			}
+
+			if ($params->get('show_users', '1'))
+			{
+				$amount = self::countUsers();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-users',
+					'amount' => self::countUsers(),
+					'link'   => Route::_('index.php?option=com_users'),
+					'text'   => Text::plural('MOD_QUICKICON_USERS_MANAGER', $amount),
+					'access' => array('core.manage', 'com_users'),
+					'image2'  => 'fa fa-plus',
+					'link2'   => Route::_('index.php?option=com_users&task=user.add'),
+					'text2' => Text::_('MOD_QUICKICON_USERS_ADD'),
+					'access2' => array('core.create', 'com_users')
+				];
+			}
+				
+			if ($params->get('show_menuItems', '1'))
+			{
+				$amount = self::countMenuItems();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-list',
+					'amount' => self::countMenuItems(),
+					'link'   => Route::_('index.php?option=com_menus'),
+					'access' => array('core.manage', 'com_menus'),
+					'image2'  => 'fa fa-plus',
+					'text'   => Text::plural('MOD_QUICKICON_MENUITEMS_MANAGER', $amount),
+					'link2'   => Route::_('index.php?option=com_menus&task=item.add'),
+					'text2' => Text::_('MOD_QUICKICON_MENUITEMS_ADD'),
+					'access2' => array('core.create', 'com_menus')
+				];
+			}
+
+			if ($params->get('show_articles', '1'))
+			{
+				$amount = self::countArticles();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-file-o',
+					'amount' => self::countArticles(),
+					'link'   => Route::_('index.php?option=com_content&view=articles'),
+					'text'   => Text::plural('MOD_QUICKICON_ARTICLE_MANAGER', $amount),
+					'access' => array('core.manage', 'com_content'),
+					'image2'  => 'fa fa-plus',
+					'link2'   => Route::_('index.php?option=com_content&task=article.add'),
+					'text2' => Text::_('MOD_QUICKICON_ARTICLE_ADD'),
+					'access2' => array('core.create', 'com_article')
+				];
+			}
+		
+			if ($params->get('show_categories', '1'))
+			{
+				$amount = self::countArticleCategories();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-folder',
+					'amount' => self::countArticleCategories(),
+					'link'   => Route::_('index.php?option=com_categories'),
+					'text'   => Text::plural('MOD_QUICKICON_CATEGORY_MANAGER', $amount),
+					'access' => array('core.manage', 'com_categories'),
+					'link2'   => Route::_('index.php?option=com_categories&task=category.add'),
+					'text2' => Text::_('MOD_QUICKICON_CATEGORY_ADD'),
+					'access2' => array('core.create', 'com_categories')
+				];
+			}
+
+			if ($params->get('show_media', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-image',
+					'link'   => Route::_('index.php?option=com_media'),
+					'text'   => Text::_('MOD_QUICKICON_MEDIA_MANAGER'),
+					'access' => array('core.manage', 'com_media')
+				];
+			}
+
+			if ($params->get('show_modules', '1'))
+			{
+				$amount = self::countModules();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-th',
+					'amount' => self::countModules(),
+					'link'   => Route::_('index.php?option=com_modules'),
+					'text'   => Text::plural('MOD_QUICKICON_MODULE_MANAGER', $amount),
+					'access' => array('core.manage', 'com_modules')
+				];
+			}
+
+			if ($params->get('show_plugins', '1'))
+			{
+				$amount = self::countPlugins();
+
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-plug',
+					'amount' => self::countPlugins(),
+					'link'   => Route::_('index.php?option=com_plugins'),
+					'text'   => Text::plural('MOD_QUICKICON_PLUGIN_MANAGER', $amount),
+					'access' => array('core.manage', 'com_plugins')
+				];
+			}
+
+			if ($params->get('show_templates', '1'))
+			{
+				self::$buttons[$key][] = [
+					'image'  => 'fa fa-pencil',
+					'amount' => self::countTemplates(),
+					'link'   => Route::_('index.php?option=com_templates&client_id=0'),
+					'text'   => Text::_('MOD_QUICKICON_TEMPLATES'),
+					'access' => array('core.admin', 'com_templates'),
+					// ToDo get the active default template and overrides (,self::getTplOverride)
+					'link2'   => Route::_('index.php?option=com_templates&amp;task=style.edit'),
+					'text2' => Text::_('MOD_QUICKICON_TEMPLATES_OVERRIDES'),
+					'access2' => array('core.edit', 'com_templates')
+				];
+			}
+
+
+			return self::$buttons[$key];
+		}
 	}
+	
+	/**
+	 * Method to get the number of installed and published extensions (no plugins).
+	 * 
+	 * @return  integer  The amount of published modules in frontend
+	 *
+	 * @since   4.0
+	 */
+	private static function countExtensions()
+	{
+		$app = Factory::getApplication();
+
+		$model = $app->bootComponent('com_installer')->getMVCFactory()
+			->createModel('Manage', 'Administrator', ['ignore_request' => true]);
+		
+		// Set the Start and Limit to 'all'
+		$model->setState('list.start', 0);
+		$model->setState('list.limit', 0);
+		$model->setState('filter.published', 1);
+
+		return  count($model->getItems());
+	}	
 	
 	/**
 	 * Method to get the number of published modules in frontend.
@@ -257,9 +316,7 @@ abstract class QuickIconHelper
 
 		$model = $app->bootComponent('com_modules')->getMVCFactory()
 			->createModel('Modules', 'Administrator', ['ignore_request' => true]);
-
-		$model->setState('list.select', '*');
-
+		
 		// Set the Start and Limit to 'all'
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', 0);
@@ -278,13 +335,10 @@ abstract class QuickIconHelper
 	private static function countArticles()
 	{
 		$app = Factory::getApplication();
-		
+	
 		// Get an instance of the generic articles model (administrator)
 		$model = $app->bootComponent('com_content')->getMVCFactory()
 			->createModel('Articles', 'Administrator', ['ignore_request' => true]);
-
-		// Count IDs
-		$model->setState('list.select', 'a.id');
 
 		// Set the Start and Limit to 'all'
 		$model->setState('list.start', 0);
@@ -308,9 +362,6 @@ abstract class QuickIconHelper
 		// Get an instance of the menuitems model (administrator)
 		$model = $app->bootComponent('com_menus')->getMVCFactory()->createModel('Items', 'Administrator', ['ignore_request' => true]);
 
-		// Count IDs
-		$model->setState('list.select', 'a.id');
-
 		// Set the Start and Limit to 'all'
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', 0);
@@ -333,8 +384,6 @@ abstract class QuickIconHelper
 
 		$model = $app->bootComponent('com_users')->getMVCFactory()->createModel('Users', 'Administrator', ['ignore_request' => true]);
 
-		$model->setState('list.select', '*');
-
 		// Set the Start and Limit to 'all'
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', 0);
@@ -355,9 +404,7 @@ abstract class QuickIconHelper
 		$app = Factory::getApplication();
 
 		$model = $app->bootComponent('com_plugins')->getMVCFactory()->createModel('Plugins', 'Administrator', ['ignore_request' => true]);
-
-		$model->setState('list.select', '*');
-
+		
 		// Set the Start and Limit to 'all'
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', 0);
@@ -378,8 +425,6 @@ abstract class QuickIconHelper
 		$app = Factory::getApplication();
 		
 		$model = $app->bootComponent('com_categories')->getMVCFactory()->createModel('Categories', 'Administrator', ['ignore_request' => true]);
-
-		$model->setState('list.select', 'a.id');
 
 		$model->setState('list.start', 0);
 		$model->setState('list.limit', 0);
--- layouts/joomla/quickicons/icon.php
+++ layouts/joomla/quickicons/icon.php
@@ -16,57 +16,70 @@ $id      = empty($displayData['id']) ? '' : (' id="' . $displayData['id'] . '"')
 $target  = empty($displayData['target']) ? '' : (' target="' . $displayData['target'] . '"');
 $onclick = empty($displayData['onclick']) ? '' : (' onclick="' . $displayData['onclick'] . '"');
 
+// Image above a number is small, a single image is big
+$imgsize = '';
+if (!empty($displayData['image']))
+{ 
+	$imgsize = isset($displayData['amount']) ? 'small' : 'big';
+}
+
 // The title for the link (a11y)
 $title   = empty($displayData['title']) ? '' : (' title="' . $this->escape($displayData['title']) . '"');
-$add	  = empty($displayData['addwhat']) ? '' : $displayData['addwhat'];
-// The information
+$specialclass  = empty($displayData['class']) ? '' : ('  ' . $this->escape($displayData['class']) . '"');
 $text    = empty($displayData['text']) ? '' : ('<span class="j-links-link">' . $displayData['text'] . '</span>');
 
-$class = '';
+// Additinal button
+$text2    = empty($displayData['text2']) ? '' : ($displayData['text2']);
+$title2  = empty($displayData['title2']) ? '' : (' title="' . $this->escape($displayData['title2']) . '"');
+$add	  = empty($displayData['name2']) ? '' : $displayData['name2'];
 
+// For update buttons via plugins
+$class    = '';
 if ($id !== '')
 {
 	$class = ($displayData['id'] === 'plg_quickicon_joomlaupdate'
 		|| $displayData['id'] === 'plg_quickicon_extensionupdate'
+		|| $displayData['id'] === 'plg_quickicon_privacycheck'
 		|| $displayData['id'] === 'plg_quickicon_overridecheck') ? ' class="pulse"' : '';
 }
 
 ?>
-<li class="col mb-3 d-flex <?php echo !empty($displayData['linkadd']) ? 'flex-column' : ''; ?>">
-	<a <?php echo $id . $class; ?> href="<?php echo $displayData['link']; ?>"<?php echo $target . $onclick . $title; ?>>
 
-		<?php if (isset($displayData['amount'])): ?>
-			<div class="quickicon-amount">
-				<?php
-				$amount = (int) $displayData['amount'];
-				if ($amount <  100000):
-					echo $amount;
-				else:
-					echo floor($amount / 1000) . '<span class="thsd">' . $amount % 1000 . '</span>';
-				endif;
-				?>
-			</div>
-		<?php elseif (isset($displayData['image'])): ?>
-			<div class="quickicon-icon d-flex align-items-end">
-				<div class="<?php echo $displayData['image']; ?>" aria-hidden="true"></div>
-			</div>
-		<?php endif; ?>
-		<?php // Name indicates the component
-			if (isset($displayData['name'])): ?>
-			<div class="quickicon-name d-flex align-items-end"><?php echo htmlspecialchars($displayData['name']); ?></div>
-		<?php endif; ?>
-		<?php // Information or action
-			if (isset($displayData['text'])): ?>
-				<div class="quickicon-text d-flex align-items-center"><?php echo $text; ?></div>
-		<?php endif; ?>
-	</a>
-	<?php // Add the link to the edit-form
-		if (!empty($displayData['linkadd'])): ?>
-			<a class="btn-block text-center quickicon-linkadd j-links-link py-1" href="<?php echo $displayData['linkadd']; ?>">
-				<span class="fa fa-plus mr-2" aria-hidden="true"></span>
-				<span class="sr-only"><?php echo Text::sprintf('MOD_QUICKICON_ADD_NEW', $add); ?></span>
-				<span aria-hidden="true"><?php echo $add; ?></span>
+		<li class="col mb-3 d-flex <?php echo !empty($displayData['link2']) ? 'flex-column' : ''; ?><?php echo $specialclass; ?>">
+		<?php // The big button with image, [amount], text ?>
+		<a <?php echo $id . $class; ?> href="<?php echo $displayData['link']; ?>"<?php echo $target . $onclick . $title; ?>>
+
+			<?php if (isset($displayData['image'])): ?>
+				<div class="<?php echo $imgsize;?> d-flex align-items-end">
+					<div class="<?php echo $displayData['image']; ?>" aria-hidden="true"></div>
+				</div>
+			<?php endif; ?>
+
+			<?php if (isset($displayData['amount'])): ?>
+				<div class="quickicon-amount">
+					<?php if (isset($displayData['ajaxurl'])):?>
+						<span class="fa fa-spinner quickicon-counter"  data-url="<?php echo $displayData['ajaxurl']; ?>"></span>
+					<?php else: 
+						$amount = (int) $displayData['amount'];
+						if ($amount <  100000):
+							echo $amount;
+						else:
+							echo floor($amount / 1000) . '<span class="thsd">' . $amount % 1000 . '</span>';
+						endif;
+					?>
+					<?php endif; ?>
+				</div>
+			<?php endif; ?>
+
+			<?php if (isset($displayData['text'])): ?>
+					<div class="quickicon-text d-flex align-items-center"><?php echo $text; ?></div>
+			<?php endif; ?>
+		</a>
+		<?php // The small button with image2, text 2 ?>
+		<?php if (!empty($displayData['link2'])): ?>
+			<a class="quickicon-linkadd j-links-link mt-3" href="<?php echo $displayData['link2']; ?>">
+				<span class="btn btn-link fa fa-plus mr-2" aria-hidden="true"></span>
+				<span><?php echo $displayData['text2']; ?></span>
 			</a>
-	<?php endif; ?>
-</li>
-
+		<?php endif; ?>
+	</li>
